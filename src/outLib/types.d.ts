5bb8e54d6b39bdfca76abb23aaadb187ab3d0640
grif <b_evgeny@ukr.net>
2023-03-27 21:46:33 +0300

    ver 1.0.2

--- src/outLib/types.d.ts
+++ src/outLib/types.d.ts
@@ -1,5 +1,4 @@
-import {IListener, ISubscriptionLike} from "evg_observable/src/outLib/Types";
-
+import { IListener, ISubscriptionLike } from "evg_observable/src/outLib/Types";
 export type IEventHistory<T> = {
     state: T;
     maxHistorySize: number;
@@ -8,18 +7,16 @@ export type IEventHistory<T> = {
     stateForHistory(state: T): void;
     isHistoryIncludes(state: T): boolean;
     getHistory(): T[];
-}
-
+};
 export type IHistoryArr<T> = {
     maxSize: number;
     size: number;
     push(state: T): T | undefined;
     getArr(): T[];
     isIncludes(state: T): boolean;
-}
-
+};
 export type HistoryNode<T> = {
     parent: HistoryNode<T> | undefined;
     child: HistoryNode<T> | undefined;
     value: T;
-}
+};
